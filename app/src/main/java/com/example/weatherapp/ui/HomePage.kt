25293501c958078a6ea9e79cffd8d2688de31ae8
sxellyn <suellyngomes@live.com>
2026-07-01 10:38:12 -0300

    Pratica 09 - parte 01

--- app/src/main/java/com/example/weatherapp/ui/HomePage.kt
+++ app/src/main/java/com/example/weatherapp/ui/HomePage.kt
@@ -12,19 +12,18 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.LocationOn
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.weatherapp.R
 import com.example.weatherapp.model.Forecast
 import com.example.weatherapp.model.Weather
 import com.example.weatherapp.viewmodel.MainViewModel
@@ -51,10 +50,11 @@ fun HomePage(modifier: Modifier = Modifier, viewModel: MainViewModel) {
             }
         } else {
             Row {
-                Icon(
-                    imageVector = Icons.Filled.AccountBox,
-                    contentDescription = "Localized description",
-                    modifier = modifier.size(150.dp)
+                AsyncImage(
+                    model = viewModel.weather(viewModel.city!!).imgUrl,
+                    modifier = modifier.size(140.dp),
+                    error = painterResource(id = R.drawable.loading),
+                    contentDescription = "Imagem"
                 )
                 Column {
                     Spacer(modifier = modifier.size(12.dp))
@@ -96,10 +96,11 @@ fun ForecastItem(
             .clickable(onClick = { onClick(forecast) }),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Icon(
-            imageVector = Icons.Filled.LocationOn,
-            contentDescription = "Localized description",
-            modifier = modifier.size(48.dp)
+        AsyncImage(
+            model = forecast.imgUrl,
+            modifier = modifier.size(70.dp),
+            error = painterResource(id = R.drawable.loading),
+            contentDescription = "Imagem"
         )
         Spacer(modifier = modifier.size(16.dp))
         Column {
--- app/src/main/java/com/example/weatherapp/ui/ListPage.kt
+++ app/src/main/java/com/example/weatherapp/ui/ListPage.kt
@@ -3,7 +3,6 @@ package com.example.weatherapp.ui
 import android.app.Activity
 import android.widget.Toast
 import androidx.activity.compose.LocalActivity
-
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -16,15 +15,16 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.rounded.FavoriteBorder
-import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.weatherapp.R
 import com.example.weatherapp.model.City
 import com.example.weatherapp.model.Weather
 import com.example.weatherapp.ui.nav.Route
@@ -47,9 +47,11 @@ fun CityItem(
             .clickable { onClick() },
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Icon(
-            Icons.Rounded.FavoriteBorder,
-            contentDescription = ""
+        AsyncImage(
+            model = weather.imgUrl,
+            modifier = modifier.size(75.dp),
+            error = painterResource(id = R.drawable.loading),
+            contentDescription = "Imagem"
         )
 
         Spacer(modifier = Modifier.size(12.dp))
